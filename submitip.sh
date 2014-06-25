d742a2b2cddbed65180e1b2a206a9045856a78fa
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-06-25 17:39:55 +0100

    remove old shell script

--- submitip.sh
+++
@@ -1,9 +0,0 @@
-machine=$(hostname)
-ip=$(lynx --dump http://ipecho.net/plain)	# Bit shit
-ip=$(echo $ip | tr -d " ")
-url="http://www.ajrobinson.org/ip.php?machine=${machine}&ip=${ip}"
-
-echo "   Dumping: $url"
-echo 
-
-lynx --dump ${url}
