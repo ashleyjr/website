90a73a5d54ef2a5080a0213a6798b36cdf902f7c
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-09-01 17:59:46 +0100

    samurai update

--- education/projects/samurai/samurai.php
+++ education/projects/samurai/samurai.php
@@ -34,11 +34,28 @@
 
    <body>
 
-   <h2>SAMURAI: Sixteen bit ARM and MIPS Unified RISC Architecture with Interrupts</h2>
-	  <img src="samurai.jpg" alt="samurai" target="_blank">
-	  
+   <h2>SAMURAI: Sixteen bit Arm and Mips Unified Risc Architecture with Interrupts</h2>
+	  <img src="samurai.jpg" alt="samurai" target="_blank"><br>
 
-	<h3><a href="ProgGuide.pdf" target="_blank">Programmer's Guide</a> </h3>
+	Samurai is a novel microprocessor unit in full custom 0.35um CMOS.<br>
+	All cells were created by myself along with three team members<br>
+	The reaming work was done by myself and two of the original members with a fourth external member. <br>
+	<br>
+	The processor was the only one produced, out of the ten teams made from that year of students, to implement interrupts.<br>
+	Only one is fabricated each year and we came in second place to a team with a much smaller design (our 4.61 sq mm against their 2.79 sq mm).<br>
+	They also implemented a multiply instruction whereas we chose to invest our efforts in a multi-bit shifter and interrupts.<br>
+	Our design is ready for fabrication but unfortunately will remain just a design.<br>
+	A large shot of the floor plan lives on my bedroom wall. <br>
+	Myself and my team mates are very proud.<br>
+	<br>
+	We have a verified our custom instruction set and in the code you will also find an assembler.<br>
+	Maybe one day I'll port a C complier for our chip and save up to send it off for fabrication.<br>
+	Who knows, maybe on day home fabs will be just as common as 3D printers?<br>
+
+	<h3><a href="Subroutines.pdf" target="_blank">Subroutines:</a> My initial research report.</h3>
+	<h3><a href="ProgGuide.pdf" target="_blank">Programmer's Guide:</a> If you want to write some programs.</h3>
+	<h3><a href="Report.pdf" target="_blank">Report:</a> Submitted to the University</h3>
+	<h3><a href="code.zip" target="_blank">Code:</a>It works.</h3>
 
 
 
