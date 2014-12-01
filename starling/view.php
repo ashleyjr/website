4906f0acb6da8d4e2dc1557dcfcb743b17eb9365
Ashley J. Robinson <ashley181291@gmail.com>
2014-12-01 23:11:13 +0000

    better tabulation

--- starling/view.php
+++ starling/view.php
@@ -270,65 +270,68 @@
 		$xml = new SimpleXMLElement(file_get_contents($filename));
 		$num = $xml->count();
 
-			
+        if( isset($_GET['see_closed'])){
+		    echo 
+      		    "<table>
+      		   	    <tr>
+					    <th>Code</th>
+					    <th>Title</th>
+	  		  		    <th>Detail</th>
+					    <th>Murmation</th>
+					    <th>Status</th>
+					    <th>Change Status</th>	
+					    <th>Edit</th>	
+				    </tr>";
 
-		echo 
-      		"<table>
-      		   	<tr>
-					<th>Code</th>
-					<th>Title</th>
-	  		  		<th>Detail</th>
-					<th>Murmation</th>
-					<th>Status</th>
-					<th>Change Status</th>	
-					<th>Edit</th>	
-				</tr>";
+		    for($i=($num-1);$i>-1;$i--){
 
-		for($i=($num-1);$i>-1;$i--){
-			
-         if( isset($_GET['see_closed'])){
-            // New row with colour depedent on status
-			   if($xml->entry[$i]->status == "open"){
-			   	echo '<tr bgcolor="#FFCC33">';
-			   }else{
-			   	echo '<tr bgcolor="#FF3333">';
-			   }
+                // New row with colour depedent on status
+			    if($xml->entry[$i]->status == "open"){
+			   	    echo '<tr bgcolor="#FFCC33">';
+			    }else{
+			   	    echo '<tr bgcolor="#FF2222">';
+			    }
 			   
-			   echo "<td>".$xml->entry[$i]->code."</td>";   
-			   echo "<td>".$xml->entry[$i]->title."</td>";   
-	 		   echo "<td>".$xml->entry[$i]->detail."</td>";    
-	 		   echo "<td>".$xml->entry[$i]->murmation."</td>";
-			   echo "<td>".$xml->entry[$i]->status."</td>";
-			   if($xml->entry[$i]->status == "open"){
-			   	echo "<td><a href='view.php?close=".$xml->entry[$i]->code."'>Close</a></td>";
-			   }else{
-			   	echo "<td><a href='view.php?reopen=".$xml->entry[$i]->code."'>Reopen</a></td>";
-			   }
-			   echo "<td><a href='view.php?edit=".$xml->entry[$i]->code."'>Edit</a></td>";
-            echo "</tr>";				
-         }else{
-            if($xml->entry[$i]->status == "open"){ 
-               // New row with colour depedent on status
-			      if($xml->entry[$i]->status == "open"){
-			      	echo '<tr bgcolor="#FFCC33">';
-			      }else{
-			      	echo '<tr bgcolor="#FF3333">';
-			      }
-			      
-			      echo "<td>".$xml->entry[$i]->code."</td>";   
-			      echo "<td>".$xml->entry[$i]->title."</td>";   
-	 		      echo "<td>".$xml->entry[$i]->detail."</td>";    
-	 		      echo "<td>".$xml->entry[$i]->murmation."</td>";
-			      echo "<td>".$xml->entry[$i]->status."</td>";
-			      if($xml->entry[$i]->status == "open"){
-			      	echo "<td><a href='view.php?close=".$xml->entry[$i]->code."'>Close</a></td>";
-			      }else{
-			      	echo "<td><a href='view.php?reopen=".$xml->entry[$i]->code."'>Reopen</a></td>";
-			      }
-			      echo "<td><a href='view.php?edit=".$xml->entry[$i]->code."'>Edit</a></td>";
-               echo "</tr>";				
-           }
-         }
+			    echo "<td>".$xml->entry[$i]->code."</td>";   
+			    echo "<td>".$xml->entry[$i]->title."</td>";   
+	 		    echo "<td>".$xml->entry[$i]->detail."</td>";    
+	 		    echo "<td>".$xml->entry[$i]->murmation."</td>";
+			    echo "<td>".$xml->entry[$i]->status."</td>";
+			    if($xml->entry[$i]->status == "open"){
+			   	    echo "<td><a href='view.php?close=".$xml->entry[$i]->code."'>Close</a></td>";
+			    }else{
+			   	    echo "<td><a href='view.php?reopen=".$xml->entry[$i]->code."'>Reopen</a></td>";
+			    }
+			    echo "<td><a href='view.php?edit=".$xml->entry[$i]->code."'>Edit</a></td>";
+            echo "</tr>";	
+		    }
+        }else{
+            echo 
+          	    "<table>
+      		   	    <tr>
+					    <th>Code</th>
+					    <th>Title</th>
+	  		  		    <th>Detail</th>
+					    <th>Murmation</th>
+					    <th>Close</th>	
+					    <th>Edit</th>	
+				    </tr>";
+
+		    for($i=($num-1);$i>-1;$i--){
+                if($xml->entry[$i]->status == "open"){
+			   
+			        echo "<td>".$xml->entry[$i]->code."</td>";   
+			        echo "<td>".$xml->entry[$i]->title."</td>";   
+	 		        echo "<td>".$xml->entry[$i]->detail."</td>";    
+	 		        echo "<td>".$xml->entry[$i]->murmation."</td>";
+
+    		   	    echo "<td><a href='view.php?close=".$xml->entry[$i]->code."'>Close</a></td>";
+		
+
+			        echo "<td><a href='view.php?edit=".$xml->entry[$i]->code."'>Edit</a></td>";
+                    echo "</tr>";	
+                }
+		    }
 		}
 		echo "</table>";
 	}	
