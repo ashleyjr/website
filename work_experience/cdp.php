266c3cfd7c82305bcc808c0668c71fbe428b6a6a
Ashley J. Robinson <ashley181291@gmail.com>
2015-07-18 20:44:40 +0100

    added cdp experience

---
+++ work_experience/cdp.php
@@ -0,0 +1,59 @@
+<html>
+   <head>
+      <title>cdp</title>
+         <style type="text/css">
+         body {
+	            font-family:Times New Roman;
+	            font-size:13pt;
+	            margin:30px;
+               background-color:#FFFFFF;
+               color:#8F590D;
+            }
+			a:link 
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:visited
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:hover 
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:active 
+         	{
+         	   COLOR: #8F590D;
+         	}
+            img{
+               width: auto; /* you can use % */
+               height: 250px;
+            }
+      </style> 
+   </head>
+
+   <body>
+      <h2>Cambridge Design Partnership</h2>
+      
+      Please visit <a href="http://www.cambridge-design.co.uk/" target="_blank">www.cambridge-design.co.uk</a> to find information on the company.
+
+      <p>
+     A technology and product design consultancy. Working on many cross discipline projects but remaining
+    focused on the application of electronics; typically in consumer products. Core skills applied include
+    analogue circuit design, PCB design and digital hardware design with concurrent low level software
+    development. Additional skills exercised range from basic mechanical engineering to high level software
+    development.
+      </p>
+      <p>
+      I've also published a few blogs during my time at CDP which can be found on the website...
+      <ul>
+      <li><a href="http://www.cambridge-design.co.uk/news-and-articles/blog/meet-ashley-our-near-field-communications-expert" target="_blank">Meet the team</a>
+       <li><a href="http://www.cambridge-design.co.uk/news-and-articles/blog/exploding_batteries" target="_blank">Safe battery charging</a>
+      </ul>
+      </p>
+
+      <h3>Duration: 09/2014 - Present</h3>
+      
+
+   </body>
+
