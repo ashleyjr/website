32c89b6a2072d396a453540f3fa2b53a321bfcea
Ashley J. Robinson <ashley181291@gmail.com>
2015-07-18 21:41:12 +0100

    IF to github

--- verilog/verilog.php
+++ verilog/verilog.php
@@ -1,31 +1,31 @@
 <html>
-   
-   <head>
+	
+	<head>
       <title>bio</title>
       <style type="text/css">
          body {
-               font-family:Times New Roman;
-               font-size:13pt;
-               margin:30px;
+	            font-family:Times New Roman;
+	            font-size:13pt;
+	            margin:30px;
                background-color:#FFFFFF;
                color:#8F590D;
             }
-         a:link 
-            {
-               COLOR: #8F590D;
-            }
-            a:visited
-            {
-               COLOR: #8F590D;
-            }
-            a:hover 
-            {
-               COLOR: #8F590D;
-            }
-            a:active 
-            {
-               COLOR: #8F590D;
-            }
+			a:link 
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:visited
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:hover 
+         	{
+         	   COLOR: #8F590D;
+         	}
+         	a:active 
+         	{
+         	   COLOR: #8F590D;
+         	}
             img{
                width: auto; /* you can use % */
                height: 250px;
@@ -34,9 +34,19 @@
    </head>
    <body>
     <?php
-        $homepage = file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/fir_filter/fir_filter.v');
-        echo $homepage;
+
+        
+        
+        echo nl2br(file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/list.txt'));
+        
+        $lines = file('https://raw.githubusercontent.com/ashleyjr/Verilog/master/list.txt', FILE_IGNORE_NEW_LINES);
+
+        
+        foreach($lines as $line){
+            echo nl2br(file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/'. $line ));
+        }
+
+
     ?>
    </body>
 
-</html>
