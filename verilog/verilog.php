8ee641267ee0e2bb4be949709250536acde60675
Ashley Robinson <ashley181291@gmail.com>
2016-02-28 06:11:18 -0700

    using highlight.js to make verilog look better

--- verilog/verilog.php
+++ verilog/verilog.php
@@ -30,31 +30,30 @@
                width: auto; /* you can use % */
                height: 250px;
             }
+      
       </style>
+      <meta charset="utf-8">
+      <title>highlight.js demo</title>
+      <link rel="stylesheet" href="default.css">
    </head>
    <body>
-    <?php
+      <script src="highlight.pack.js"></script>
+      <script>hljs.initHighlightingOnLoad();</script>
+      <?php
 
          if(isset($_GET['file'])){ 
             $loc = 'https://raw.githubusercontent.com/ashleyjr/Verilog/master/'.$_GET['file'];
+            
+            
             $file =  file_get_contents($loc);
-            echo nl2br($file);
+            echo "<pre><code class='verilog'>".$file."</code></pre>";
          }else{
             $list = file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/list.txt');
             echo "test";
             foreach(preg_split("/((\r?\n)|(\r\n?))/", $list) as $line){
                echo "<a href='verilog.php?file=".$line."'>".$line."</a><br>";
             } 
-         }
-        
-       # $lines = file('https://raw.githubusercontent.com/ashleyjr/Verilog/master/list.txt', FILE_IGNORE_NEW_LINES);
-
-       # 
-       # foreach($lines as $line){
-       #     echo nl2br(file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/'. $line ));
-       # }
-
-
+         } 
     ?>
    </body>
 
