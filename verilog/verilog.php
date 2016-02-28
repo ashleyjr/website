a215e1e3e4691e325450b7c27abfd356c90610a8
Ashley Robinson <ashley181291@gmail.com>
2016-02-28 07:17:40 -0700

    loading files in hljs

--- verilog/verilog.php
+++ verilog/verilog.php
@@ -1,60 +1,112 @@
 <html>
-	
 	<head>
-      <title>bio</title>
-      <style type="text/css">
-         body {
-	            font-family:Times New Roman;
-	            font-size:13pt;
-	            margin:30px;
-               background-color:#FFFFFF;
-               color:#8F590D;
-            }
-			a:link 
-         	{
-         	   COLOR: #8F590D;
-         	}
-         	a:visited
-         	{
-         	   COLOR: #8F590D;
-         	}
-         	a:hover 
-         	{
-         	   COLOR: #8F590D;
-         	}
-         	a:active 
-         	{
-         	   COLOR: #8F590D;
-         	}
-            img{
-               width: auto; /* you can use % */
-               height: 250px;
-            }
-      
-      </style>
-      <meta charset="utf-8">
-      <title>highlight.js demo</title>
-      <link rel="stylesheet" href="default.css">
-   </head>
-   <body>
-      <script src="highlight.pack.js"></script>
-      <script>hljs.initHighlightingOnLoad();</script>
+      <title>bio</title> 
       <?php
-
-         if(isset($_GET['file'])){ 
-            $loc = 'https://raw.githubusercontent.com/ashleyjr/Verilog/master/'.$_GET['file'];
-            
-            
+         if(isset($_GET['file'])){  
+            echo '<style type="text/css">
+               body {
+	               margin:30px;
+                  background-color:#221a0f;
+                  color:#d6baad;
+               } 
+               .hljs-comment,
+               .hljs-quote {
+                   color: #d6baad;
+               }
+               .hljs-variable,
+                  .hljs-template-variable,
+                  .hljs-tag,
+                  .hljs-name,
+                  .hljs-selector-id,
+                  .hljs-selector-class,
+                  .hljs-regexp,
+                  .hljs-meta {
+                      color: #dc3958;
+               }
+               .hljs-number,
+                  .hljs-built_in,
+                  .hljs-builtin-name,
+                  .hljs-literal,
+                  .hljs-type,
+                  .hljs-params,
+                  .hljs-deletion,
+                  .hljs-link {
+                      color: #f79a32;
+               }
+               .hljs-title,
+                  .hljs-section,
+                  .hljs-attribute {
+                      color: #f06431;
+               }
+               .hljs-string,
+                  .hljs-symbol,
+                  .hljs-bullet,
+                  .hljs-addition {
+                      color: #889b4a;
+               }
+               .hljs-keyword,
+                  .hljs-selector-tag,
+                  .hljs-function {
+                      color: #98676a;
+               }
+               .hljs {
+                   display: block;
+                    overflow-x: auto;
+                    background: #221a0f;
+                     color: #d3af86;
+                     padding: 0.5em;
+               }
+               .hljs-emphasis {
+                   font-style: italic;
+               }
+               .hljs-strong {
+                   font-weight: bold;
+               } 
+            </style>';
+            $loc = 'https://raw.githubusercontent.com/ashleyjr/Verilog/master/'.$_GET['file']; 
             $file =  file_get_contents($loc);
             echo "<pre><code class='verilog'>".$file."</code></pre>";
          }else{
+            echo '
+               <style type="text/css">
+                  body {
+	                  font-family: monospace;
+	                  margin:30px;
+                     background-color:#221a0f;
+                     color:#d6baad;
+                  }
+			         a:link 
+         	      {
+                     COLOR: #d6baad;
+                  }
+			         a:visited 
+         	      {
+         	         COLOR: #d6baad;
+         	      }
+         	      a:hover 
+         	      {
+         	         COLOR: #d6baad;
+         	      }
+         	      a:active 
+         	      {
+         	         COLOR: #d6baad;
+         	      }
+                  img{
+                     width: auto; /* you can use % */
+                     height: 250px;
+                  }
+               </style>
+            ';
             $list = file_get_contents('https://raw.githubusercontent.com/ashleyjr/Verilog/master/list.txt');
-            echo "test";
             foreach(preg_split("/((\r?\n)|(\r\n?))/", $list) as $line){
                echo "<a href='verilog.php?file=".$line."'>".$line."</a><br>";
             } 
          } 
-    ?>
+      ?> 
+   </head>
+   <body>
+      <script src="highlight.pack.js"></script>
+      <script>hljs.initHighlightingOnLoad();</script>
    </body>
 
 </html>
