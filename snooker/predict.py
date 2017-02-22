687796beb5e04a796406374d438068e2263f9c5d
Ashley J. Robinson <ashley181291@gmail.com>
2017-02-22 22:43:13 +0000

    tidy up xml

--- snooker/predict.py
+++ snooker/predict.py
@@ -5,12 +5,8 @@ import csv
 from time import gmtime, strftime
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+import lxml.etree as etree
 
-
-def xml_format(f):
-    rough_string = ET.tostring(f, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return minidom.parseString(rough_string).toprettyxml(indent="\t")
 
 def main(rank_file, match_file, predict_xml):
     """ Main """
@@ -21,9 +17,7 @@ def main(rank_file, match_file, predict_xml):
         p.write("<data>\n</data>\n")
         p.close()
 
-
     """ Draw on data from rank and upcoming match """#
-
     doc = ET.parse(predict_xml)
     root_node = doc.getroot()
     m = ET.SubElement(doc.getroot(), "match")
@@ -34,51 +28,15 @@ def main(rank_file, match_file, predict_xml):
     tree = ET.ElementTree(root_node)
     tree.write(predict_xml)
 
+    """ Tidy up xml """
     f = open(predict_xml, 'w')
-    f.write(minidom.parseString(ET.tostring(root_node, 'utf-8')).toprettyxml(indent="\t"))
+    f.write(minidom.parseString(ET.tostring(root_node, 'utf-8')).toprettyxml(indent="").replace("\n",""))
     f.close()
 
-
-
-    # p = open(predict_f, 'a+')
-    # match = csv.reader(open(match_file))
-    # for m in match:
-    #     found_a = []
-    #     found_b = []
-    #     rank = csv.reader(open(rank_file))
-    #     for r in rank:
-    #         if m[1] == r[3]:
-    #             found_a = r
-    #         if m[2] == r[3]:
-    #             found_b = r
-    #     if len(found_a):
-    #         if len(found_b):
-    #             p.write(str(strftime("%Y-%m-%d", gmtime())))
-    #             p.write(",")
-    #             p.write(m[0])
-    #             p.write(",")
-    #             p.write(found_a[0])
-    #             p.write(",")
-    #             p.write(found_a[1])
-    #             p.write(",")
-    #             p.write(found_a[2])
-    #             p.write(",")
-    #             p.write(found_a[3])
-    #             p.write(",")
-    #             p.write(found_b[0])
-    #             p.write(",")
-    #             p.write(found_b[1])
-    #             p.write(",")
-    #             p.write(found_b[2])
-    #             p.write(",")
-    #             p.write(found_b[3])
-    #             p.write(",")
-    #             p.write("A")
-    #             p.write(",")
-    #             p.write("")
-    #             p.write("\n")
-
-
+    x = etree.parse(predict_xml)
+    f = open(predict_xml, 'w')
+    f.write(etree.tostring(x, pretty_print = True))
+    f.close()
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
