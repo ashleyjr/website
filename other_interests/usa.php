bce519330ee96bca42ff97818833a70bd2222f77
Ashley Robinson <ashley181291@gmail.com>
2017-09-30 08:43:26 -0700

    few more lines on the usa

--- other_interests/usa.php
+++ other_interests/usa.php
@@ -27,13 +27,162 @@
          	}
             img{
                width: auto; /* you can use % */
-               height: 250px;
+               height: 400px;
             }
       </style> 
    </head>
 
    <body>
-      <h3>North West USA</h3>
+      <h2>North West USA</h2>
+
+      My two week trip across North West USA takes in famous cities along the West Coast, three national parks and some awesome back roads as I search for campsites.
+      Yellowstone is truly astonishing and is accompanied by a camping culture (for tents, not those awful three story RVs) far better than any other country I've had the opportunity to explore.
+      A mixture snow storms, wild fires and risk of grizzly bears swiping your picnic basket means the wild west clich&egrave holds true.
+      <p>
       <a><img src="usa.png" alt="USA route"></a> 
+
+      <h4>Day 1 - Tuesday 12/09/17</h4>
+      The 10 hour flight from Gatwick to Vancouver goes by surprisingly quickly but the jet lag hangs around a bit too long. Leaving at midday London time to arrive at 2pm Vancouver time 
+      means I'm fast asleep by 8pm and awake at 5am the next day. 
+      <p>
+      Accommodation - 6/10 - The <a href="https://www.stclairvancouver.com/" target="_blank">St Clair Hostel</a> is a great budget place to stay in Vancouver and offers private rooms at a good price.
+      <p>
+      Drink/Food - 7/10 - First craft beer of the trip at the <a href="http://donnellygroup.ca/the-lamplighter/" target="_blank">The Lamplighter</a>.  
+      <p>
+      Activities - 4/10 - Exploring the city to fight off the jet lag. 
+      <p>
+      <h4>Day 2 - Wednesday 13/09/17</h4>
+      An early start leaves me plenty of time of to explore the city and grab something to eat at Denny's before picking up the hire car just before midday. I head straight
+      for the US boarder. This turns out to be a good idea as crossing the boarder takes nearly 2 hours. I wait in a queue of foreigners hoping they don't search my car. Not
+      that I have anything to hide, I just want to get to Seattle! When my turn comes the officer is very friendly and decides my car doesn't require any investigation. He then directs 
+      me to the cashier who requests $6, on top of the $14 I've already paid for an ESTA, which is an additional charge for entering the USA by land.
+
+      Back on the I-5 (interstate 5) I make tracks towards Seattle. I get my first glimpse of the space needle from the highway and it doesn't disappoint. A very impressive building.
+      As I arrive at the hostel I realise parking is going to be an issue on this trip. An overnight stay at the car park across the road is almost as much as the hostel. The guy
+      at reception suggests I wait it out for overnight parking on the street/meter which is free! Guys in my room are nice and the top bunk is comfortable but I need to keep moving if I want 
+      to beat this jet lag. I head straight for pike place market on foot just to catch the stalls closing up but I get the picture. As sunset is approaching the space needle is the next 
+      destination to check out. A ride to the top costs $22 and experience is well worth the price. At glass wall allows passengers to see over the city as they ascend. Once at the top the 
+      360 degree view is excellent looking over Elliott Bay and neighbour city of Bellevue. 
+      The Seattle cityscape is shadowed by Mount Rainer which make it very impressive. Even more so when you discover Mount Rainer is an active volcano.
+      I later hear from another traveller that there is a 50% chance of Mount Rainer erupting within the next 30 years.
+      I'm not sure if this is true but the US government take the <a href="https://www.nps.gov/mora/learn/nature/volcanoes.htm" target="_blank">threat of an eruption</a> very seriously.
+      <p>
+      Accommodation - 6/10 - <a href="https://www.hihostels.com/hostels/hi-seattle-at-the-american-hotel/" target="_blank">HI Seattle</a> is a nice place to stay but for my visit the 
+      crowd was not your typically hostel types. More middle age couples and parents/kids than 20 somethings looking to find themselves. The vibe of a hostel is more important than 
+      a comfortable bed and a decent breakfast.
+      <p>
+      Drink/Food - 7/10 - More quality craft beer! 
+      <p>
+      Activities - 8/10 - What a great city, full of interesting sights, along with the homely feeling of being in a giant Starbucks. 
+      <p>
+       <h4>Day 3 - Thursday 14/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 4 - Friday 15/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+      <h4>Day 5 - Saturday 16/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 6 - Sunday 17/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 7 - Monday 18/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 8 - Tuesday 19/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 9 - Wednesday 20/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 10 - Thursday 21/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 11 - Friday 22/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+       <h4>Day 12 - Saturday 23/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p> 
+      <h4>Day 13 - Sunday 24/09/17</h4>
+      Waking up at the base of Humbug Mountain I pack up the tent and hit the 101 toward Portland. To make up some time I take the 38 across to the I-5 make the journey a bit quicker.
+      It's not long before I arrive in Portland. I heard from other travels and TV shows it's a city full of hipsters. I feel like that is true. The Saturday market (also open Sundays)
+       is full of quirky pieces of art and posh fast food. Food trucks have taken up permanent residency in a parking lot a short walk from the micro brewery block. Among all this locals
+      can be seen cart literal carts full of used books to sell at Powell's book store. After an evening of excellent craft beer and excellent bluegrass music by Kendl Winter at Al's Den I retire to the hostel. 
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+      <h4>Day 14 - Monday 25/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+      <h4>Day 15 - Tuesday 26/09/17</h4>
+      <p>
+      Accommodation - 
+      <p>
+      Drink/Food - 
+      <p>
+      Activities - 
+      <p>
+      
    </body>
 </html>
