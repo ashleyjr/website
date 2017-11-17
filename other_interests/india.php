ac6510b8a8ec30dc8240764865f4954591980284
Ashley Robinson <ashley181291@gmail.com>
2017-11-17 13:27:08 -0700

    day 1 india

--- other_interests/india.php
+++ other_interests/india.php
@@ -33,12 +33,43 @@
    </head>
 
    <body>
-
-
-
       <h3>Dehli to Mumbai</h3>
-      <a><img src="india.png" alt="India route"></a> 
- 
+      Me and Tom travel across central India.
 
+      <a><img src="india.png" alt="India route"></a> 
+
+      <h4>Day 1 - Wednesday 23/08/17</h4>
+      The flight from Heathrow was a joy. 
+      An almost empty British Airways flight took off late morning arriving in Dehli just before midnight.
+      Border control was awful. 
+      A mix up with visas left me stranded in front of a India border while they discussed my right to enter the country.
+      Turns out there are many ways to purchase an Indian visa legitimately even if not directly from the government. 
+      This was the first of many mix ups and slightly corrupt business transactions we would have in India. 
+
+      We started to leave the airport once stocked up on Rupees but our path was block by another border officer.
+      This time he had a machine gun instead of a stamp.
+      A small team had barricaded the door as some sort of training exercise.
+      The hordes of Dehli were not fazed by this exercise as the clung to us desperately selling their taxis.
+      Luckily we had a pre-booked taxi and made our way towards Dehli in dirty heat of Dehli.   
+
+      There is no way to put this nicely. 
+      Dehli is a shit hole.
+      Litter is absolutely everywhere you look, poverty stricken families live under bridges by road sides and stray dogs threaten you with rabies almost every step.  
+
+      <h4>Day 2 - Thursday 24/08/17</h4>
+      <h4>Day 3 - Friday 25/08/17</h4>
+      <h4>Day 4 - Saturday 26/08/17</h4>
+      <h4>Day 5 - Sunday 27/08/17</h4>
+      <h4>Day 6 - Monday 28/08/17</h4>
+      <h4>Day 7 - Tuesday 29/08/17</h4>
+      <h4>Day 8 - Wednesday 30/08/17</h4>
+      <h4>Day 9 - Thursday 31/08/17</h4>
+      <h4>Day 10 - Friday 01/09/17</h4>
+      <h4>Day 11 - Saturday 02/09/17</h4>
+      <h4>Day 12 - Sunday 03/09/17</h4>
+      <h4>Day 13 - Monday 04/09/17</h4>
+      <h4>Day 14 - Tuesday 05/09/17</h4>
+      <h4>Day 15 - Wednesday 06/09/17</h4>
+      <h4>Day 16 - Thursday 07/09/17</h4>
    </body>
 </html>
